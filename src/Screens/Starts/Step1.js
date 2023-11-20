5bd684403ce9ec1e5c5d0b3d4f2b5cde7ff405bf
Dmitriy <aua008@gmail.com>
2023-11-20 16:29:27 +0200

    change image

--- src/Screens/Starts/Step1.js
+++ src/Screens/Starts/Step1.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { SvgIcon } from '../../Svgs';
 import { CircleArrowButton, Title, StepProgress } from '../../Components/UI';
+import LogoImg from '../../assets/logo.svg';
+import styles from './start.module.css';
 
 export const Step1 = () => {
 	return (
 		<div className='screen black-bg'>
 			<div className='body' style={{ paddingTop: '10vh' }}>
-				<SvgIcon type='logo' />
-				<Title color='white' mt={30} size='m'>
-					The crypto wallet for everyone
+				<img className={styles.logo} src={LogoImg} alt='logo' />
+				<Title color='white' mt={40} size='m'>
+					The crypto wallet for
+					<br /> everyone
 				</Title>
 			</div>
 			<div className='body-bottom footer-step'>
